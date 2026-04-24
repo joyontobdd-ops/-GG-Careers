ceaf6d5e6d1ed4a64fc76ad4599dff7d8f14a9d3
joyontobdd-ops <joyontobdd-ops@users.noreply.github.com>
2026-04-24 09:56:13 +0000

    Sync full project to GitHub

--- GooglePhish-dev/Phisher/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Phisher.models import SignIn
-
-# Models
-admin.site.register(SignIn)
--- GooglePhish-dev/Phisher/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from Phisher import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('signin', views.signin, name='signin' ),
-    path('error', views.error, name='error'),
-    path('get-hits', views.get_hit_counts, name='get-hit-counts'),
-]
